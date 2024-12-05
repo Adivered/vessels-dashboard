062ad7a1c90e1554eac9cd4b7c41756c909aeb0f
Sharon Marcelo Del Giudice <smdg972@outlook.com>
2024-12-05 10:42:28 +0200

    updated script

--- src/script.js
+++ src/script.js
@@ -50,16 +50,7 @@ const setSearchFilterOptions = (jsonArray) => {
             li.textContent = key.toUpperCase().replaceAll("_", " ")
             ul.appendChild(li)
             li.addEventListener('click',() => {
-                const appendShipsToList = (jsonArray) => {
-                    const ul = document.getElementById('search-results-ul'); // get the ul element by id
-                    ul.textContent = ''; // Clear the existing list
-                    jsonArray.forEach(ship => { // Loop on each ship in jsonArray
-                        const li = document.createElement('li'); // Create a new li element
-                        li.className = 'list-group-item'; // Add a class name to the li element
-                        li.textContent = ship.ship_name; // Set the text content of the li element
-                        ul.appendChild(li); // Append the li element to the ul element
-                    });
-                };
+            
             })
         }
     });
